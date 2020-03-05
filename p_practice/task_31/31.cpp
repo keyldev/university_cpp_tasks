1e8ba585c1cb43487d4d5ba561825bb1df0507ac
zalatoff <55500552+zalatoff@users.noreply.github.com>
2020-03-05 18:31:18 +0300

    upd

--- p_practice/task_31/31.cpp
+++ p_practice/task_31/31.cpp
@@ -3,19 +3,21 @@
 #include <string>
 using namespace std;
 
-
 void show_exits(string str)
 {
 	cout << "EXITS: " << endl << str << endl;
 }
+
+
+
 string temp_str;
-void exit(string maze[], int str, int col)
+bool exit(string maze[], int str, int col)
 {
 	maze[str][col] = '.';
 
 	if (maze[str + 1][col] == ' ')
 	{
-		if (isalpha(maze[str][col - 1])) {  temp_str = temp_str + maze[str][col - 1]; maze[str][col - 1] = '*'; }
+		if (isalpha(maze[str][col - 1])) { temp_str = temp_str + maze[str][col - 1]; maze[str][col - 1] = '*'; }
 		str++; maze[str][col] = '@';
 	}
 	else if (maze[str][col + 1] == ' ')
@@ -39,24 +41,26 @@ void exit(string maze[], int str, int col)
 		}
 		col--; maze[str][col] = '@';
 	}
-	else if (maze[str][col+1] == '.')
+	else if (maze[str][col + 1] == '.')
 	{
 		maze[str][col] = '@';  col++; maze[str][col] = '@';
 	}
 	else if (maze[str + 1][col] == '.')
 	{
-
+		if (isalpha(maze[str][col + 1])) { temp_str = temp_str + " " + maze[str][col + 1]; maze[str][col + 1] = '*'; }
 		maze[str][col] = '@'; str++; maze[str][col] = '@';
 	}
 	else if (maze[str][col - 1] == '.')
 	{
+		if (isalpha(maze[str - 1][col])) { temp_str = temp_str + ' ' + maze[str - 1][col]; maze[str - 1][col]; maze[str - 1][col] = '*'; }
 		maze[str][col] = '@'; col--; maze[str][col] = '@';
 	}
 	else if (maze[str - 1][col] == '.')
 	{
+		if (isalpha(maze[str][col + 1])) { temp_str = temp_str + ' ' + maze[str][col + 1]; maze[str][col + 1] = '*'; }
 		maze[str][col] = '@'; str--; maze[str][col] = '@';
 	}
-	//Sleep(400);
+	else return false;
 	system("cls");
 	for (int i = 0; i < 25; i++) {
 		cout << maze[i] << endl;
@@ -64,7 +68,6 @@ void exit(string maze[], int str, int col)
 	show_exits(temp_str);
 	exit(maze, str, col);
 }
-
 int main()
 {
 	setlocale(LC_ALL, "ru");
@@ -97,25 +100,18 @@ int main()
 	"#         #########       #",
 	"#######E############D######"
 	};
-	for (int j = 0; j < MAZE_SIZE; j++)
-		cout << maze[j] << endl;
-
 	cout << endl;
     int str, col;
     do
     {
-        cout << endl << "Введите стартовые координаты через пробел: ";
-        cin >> str >> col;
-
+        cout << endl << "Введите стартовые координаты через пробел(№ колонки, № столбца): ";
+        cin >> col >> str;
 		if (maze[str][col] == '#' )
 			cout << "Вы не можете начать со стены." << endl;
-		else
-			break;
-
+		else break;
     } while (maze[str][col] != '\0');
 	FindExit fe;
 	//fe.mazeTrip(maze, str, col);
 	exit(maze, str, col);
-
 	return 0;
 }
